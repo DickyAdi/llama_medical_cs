69a447b8b4e620bebab64f1ed46f555a1d6475dc
root <dickyadi44@gmail.com>
2025-04-14 07:30:07 +0000

    add comment to database setup script

--- database_setup.sh
+++ database_setup.sh
@@ -3,8 +3,11 @@ apt-get update
 apt-get -y install mysql-server
 curl -fsSL https://ollama.com/install.sh | sh
 service mysql start
+#this line below are used to setup a mysql database in the cloud vm as it naturally doesn't support any DBMS
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'root';FLUSH PRIVILEGES;"
+#create the database for the app
 mysql -u root -p -e "CREATE DATABASE medical_cs;"
+#loading the dummy database
 mysql -u root -p medical_cs < medical_cs_db.sql
 pip install langchain langgraph PyMySQL mysql-connector-python PyMySQL langchain-community 'bitsandbytes==0.45.0' 'accelerate >=0.26.0' langchain-ollama langgraph-prebuilt "fastapi[all]"
 ollama serve
